83625120326559b17ad2847d8e791accb4d04875
Anish Sharma <sharmaanish023@gmail.com>
2022-09-05 18:37:38 +0545

    FIX #1 react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import "./App.css";
 import Login from "./pages/Login";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Login />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
