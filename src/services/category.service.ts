ed2ba267e41a587803eeb327f4633f741c6a6ef2
Anish Sharma <sharmaanish023@gmail.com>
2022-09-08 12:19:19 +0545

    FIX #3 Add category service

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,7 +1,19 @@
 import axios from "axios";
 
+export type CategoryData = {
+  name: string;
+};
+
 export const getCategory = async () => {
   const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/category/`, {
+    withCredentials: true,
+  });
+
+  return res;
+};
+
+export const addCategory = async (data: CategoryData) => {
+  const res = axios.post(`${import.meta.env.VITE_BASE_URL}/category/`, data, {
     withCredentials: true,
   });
 
