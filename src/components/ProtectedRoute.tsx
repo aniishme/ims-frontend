2caada17177e96b5120aab51e52a443331acf5b9
Anish Sharma <sharmaanish023@gmail.com>
2022-09-08 18:38:10 +0545

    FIX #3 Render Login component instead of Navigate

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext, AuthContextType } from "../context/AuthContext";
+import Login from "../pages/Login";
 
 function ProtectedRoute() {
   const { auth } = useContext(AuthContext) as AuthContextType;
 
-  return auth ? <Outlet /> : <Navigate to="/login" />;
+  return auth ? <Outlet /> : <Login />;
 }
 
 export default ProtectedRoute;
