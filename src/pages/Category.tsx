0f356e6c6e3d8ae150c849b8a89570b8263b9009
Anish Sharma <sharmaanish023@gmail.com>
2022-09-08 11:39:48 +0545

    FIX #3 Category page to display caetgories

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,11 +1,56 @@
-import React from "react";
+import { Button, Loader, Table } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
 import Navbar from "../components/Navbar";
+import {
+  CategoryContext,
+  CategoryContextType,
+} from "../context/CategoryContext";
 
 function Category() {
+  const { isLoading, isSuccess, data } = useContext(
+    CategoryContext
+  ) as CategoryContextType;
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      {isLoading && <Loader />}
+      {isSuccess && (
+        <div className="category">
+          <Table verticalSpacing={"md"} striped highlightOnHover>
+            <thead>
+              <tr>
+                <th>S.N</th>
+                <th>Name</th>
+                <th>No. of Items</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((category: any, index: number) => {
+                return (
+                  <tr key={category.id} className="category-list-item">
+                    <td>{index + 1}</td>
+                    <td>
+                      <Link to={`/category/${category.id}`}>
+                        {category.name}
+                      </Link>
+                    </td>
+                    <td>{category.items.length}</td>
+                    <td>
+                      <Button color="blue">Edit</Button>
+                      <Button color="red">Delete</Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
+      )}
+    </>
   );
 }
 
