3275a30a7241f36c692ef9081fcdfb85ccab8bc0
Anish Sharma <sharmaanish023@gmail.com>
2022-09-08 14:03:49 +0545

    FIX #3 edit category service

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -4,6 +4,10 @@ export type CategoryData = {
   name: string;
 };
 
+type CategoryDataWithId = CategoryData & {
+  id: number;
+};
+
 export const getCategory = async () => {
   const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/category/`, {
     withCredentials: true,
@@ -16,6 +20,20 @@ export const addCategory = async (data: CategoryData) => {
   const res = axios.post(`${import.meta.env.VITE_BASE_URL}/category/`, data, {
     withCredentials: true,
   });
+
+  return res;
+};
+
+export const editCategory = async (data: CategoryDataWithId) => {
+  const res = axios.put(
+    `${import.meta.env.VITE_BASE_URL}/category/${data.id}`,
+    {
+      name: data.name,
+    },
+    {
+      withCredentials: true,
+    }
+  );
 
   return res;
 };
