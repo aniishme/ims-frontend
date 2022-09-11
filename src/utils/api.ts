748eb892af96fdacc249a8ef1249509d479cb13a
Anish Sharma <sharmaanish023@gmail.com>
2022-09-11 18:33:31 +0545

    FIX #3 axios interceptor for unauthorized response

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,3 +4,12 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
+
+api.interceptors.response.use((response) => {
+  if (response.status === 401 || response.status === 403) {
+    const res = api.get("/auth/logout");
+    return res;
+  }
+
+  return response;
+});
