8ae39491a3b369faaab90c6a96df5d11118a9884
Anish Sharma <sharmaanish023@gmail.com>
2022-09-12 08:23:45 +0545

    FIX #3 implemented logout in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,24 @@
+import { Button } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { logout } from "../services/auth.service";
+import { useNavigate } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
 
 function Navbar() {
   const { user } = useAuth();
+  const { isSuccess, refetch } = useQuery(["logout"], logout, {
+    enabled: false,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["auth"]);
+    },
+  });
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    refetch();
+  };
   return (
     <div className="navbar">
       <div className="nav-links">
@@ -15,6 +31,10 @@ function Navbar() {
         <p>
           {user?.username} | {user?.role?.toLowerCase()}
         </p>
+
+        <Button variant="outline" color="red" onClick={handleLogout}>
+          Logout
+        </Button>
       </div>
     </div>
   );
