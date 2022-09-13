755e60fddbb506aa889ff134383e3f8e715095b6
Anish Sharma <sharmaanish023@gmail.com>
2022-09-13 16:01:53 +0545

    FIX #3 single category view

--- src/pages/CategoryView.tsx
+++ src/pages/CategoryView.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import Navbar from "../components/Navbar";
+import { getCategoryById } from "../services/category.service";
+import { useParams } from "react-router-dom";
 
 function CategoryView() {
-  return <div>Category By Id</div>;
+  const { id } = useParams();
+  const category = useQuery(["get-single-category"], () =>
+    getCategoryById({ id: id })
+  );
+  const data = category.data?.data;
+  return (
+    <>
+      <Navbar />
+      <div className="category-wrapper">
+        <h1>Name: {data?.name}</h1>
+        {data?.items.length === 0
+          ? "No Items in this category"
+          : data?.items.map((item: any) => {
+              return (
+                <ul key={item.id}>
+                  <li>{item.name}</li>
+                  <p>Price ${item.price}</p>
+                </ul>
+              );
+            })}
+      </div>
+    </>
+  );
 }
 
 export default CategoryView;
