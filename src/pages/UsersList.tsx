c11a2b315cc20e00a3244720431ca016798b5f7c
Anish Sharma <sharmaanish023@gmail.com>
2022-09-18 19:29:20 +0545

    FIX #7 delete user request

--- src/pages/UsersList.tsx
+++ src/pages/UsersList.tsx
@@ -1,14 +1,41 @@
-import { Loader, Table } from "@mantine/core";
+import { Button, Loader, Table } from "@mantine/core";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import React from "react";
 import ItemTableRow from "../components/ItemTableRow";
 import { useUsers } from "../hooks/useUsers";
+import { deleteUsers } from "../services/auth.service";
 
 function UsersList() {
   const users = useUsers();
   const usersData = users?.data?.data;
+  const {
+    mutateAsync: handleDeleteUser,
+    isError,
+    isLoading,
+    error,
+  } = useMutation(deleteUsers);
+
+  const handleDelete = (id: string) => {
+    handleDeleteUser(
+      { id: id },
+      {
+        onSuccess: () => {
+          users.refetch();
+        },
+      }
+    );
+  };
+
   console.log(usersData);
   return (
     <div className="userlist-wrapper">
+      {isError &&
+        error instanceof AxiosError &&
+        (error.message == "Network Error"
+          ? "Can't connect to the network"
+          : error.response?.data?.message)}
+      {isLoading && <Loader />}
       {users?.isLoading && <Loader />}
       {users?.isSuccess && (
         <Table verticalSpacing={"md"} striped highlightOnHover>
@@ -29,6 +56,11 @@ function UsersList() {
                   <td>{user.name}</td>
                   <td>{user.username}</td>
                   <td>{user.role}</td>
+                  <td>
+                    <Button color="red" onClick={() => handleDelete(user.id)}>
+                      Delete
+                    </Button>
+                  </td>
                 </tr>
               );
             })}
